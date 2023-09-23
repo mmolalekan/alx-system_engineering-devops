28fa48c4202b781c3132358b42904bb783d6d3ee
muhaymin <muhayminolalekan@yahoo.com>
2023-09-23 17:32:01 +0100

    task 1 and 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,6 @@
 # Using Puppet, install flask (version 2.1.0) from pip3.
 
 package { 'flask':
-    ensure      => '2.1.0',
-    provider    => 'pip3',
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
